e6b2569c9f5ca72ad7846c6e5718e315d7a71a97
Wasin Thonkaew <haxpor@gmail.com>
2018-01-10 10:01:22 +0800

    Add travis ci support

---
+++ ci/scripts/install_profiles.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+cp ../profiles/main-development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ../profiles/ex-packettunnel-development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ../profiles/ex-todaywidget-development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
