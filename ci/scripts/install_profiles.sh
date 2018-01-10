1ee44c3743410760de610de2f17a7fdb6a5a96a1
Wasin Thonkaew <haxpor@gmail.com>
2018-01-10 11:43:19 +0800

    Fix paths as used in scripts executed by travis-ci
    They still need to be relative to root directory (the same level of
    .travis.yml).

--- ci/scripts/install_profiles.sh
+++ ci/scripts/install_profiles.sh
@@ -1,5 +1,6 @@
 #/bin/sh
+# note working directory path still relative from the root as this script is executed inside .travis.yml
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ../profiles/main-development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
-cp ../profiles/ex-packettunnel-development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
-cp ../profiles/ex-todaywidget-development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./ci/profiles/main-development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./ci/profiles/ex-packettunnel-development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./ci/profiles/ex-todaywidget-development.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
