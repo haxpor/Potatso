2a2629f598257ecc7951ccda29cce5a319d28dc7
zhkufish <conghua19@126.com>
2018-01-10 17:30:38 +0800

    Update AppInitializer.swift
    getLogglyAPIKey from info.plist

--- Potatso/AppInitializer.swift
+++ Potatso/AppInitializer.swift
@@ -37,7 +37,7 @@ class AppInitializer: NSObject, AppLifeCycleProtocol {
         DDLog.add(fileLogger)
 
         let logglyLogger = LogglyLogger() // Loggy Logger
-        logglyLogger.logglyKey = "107d98a8-c275-4369-a881-b36d3564c9ef"
+        logglyLogger.logglyKey = InfoInternal.shared.getLogglyAPIKey()
         let fields = LogglyFields()
         fields.userid = User.currentUser.id
         fields.appversion = AppEnv.fullVersion
